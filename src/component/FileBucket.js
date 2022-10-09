7e51218533b1cc64d72a1603cb4582d697e09f29
sunnypaul19999 <sunnypaul19999@gmail.com>
2022-10-10 01:33:00 +0530

    file record refresh solved using timeout clearing and while loop on entry to fetchFileRecords

--- src/component/FileBucket.js
+++ src/component/FileBucket.js
@@ -9,15 +9,21 @@ export default function FileBucket() {
     const [filesRecords, setFileRecords] = useState({});
 
     const isMounted = useRef(false);
-    const refreshFileRecords = useRef({ inProgress: false })
+    const fetchFileRecordTimeouts = useRef([]);
 
     const fetchFileRecords = async () => {
-        if (isMounted.current && !refreshFileRecords.inProgress) {
+        //clear timeouts
+        while (fetchFileRecordTimeouts.current.length > 0) {
+            // console.log(fetchFileRecordTimeouts.current.length);
+            clearTimeout(fetchFileRecordTimeouts.current.pop());
+        }
+        if (isMounted.current && fetchFileRecordTimeouts.current.length == 0) {
             try {
                 const res = await axios.get(process.env.REACT_APP_SERVER_URL + '/user/file/all', {
                     withCredentials: true
                 });
                 const records = res.data;
+                // console.log(records);
                 const prepareDraft = (draft) => {
                     records.forEach(record => {
                         if (!filesRecords[record.file_record_id]) {
@@ -25,34 +31,38 @@ export default function FileBucket() {
                         }
                     });
                 }
+
                 setFileRecords(produce(prepareDraft));
 
             } catch (err) {
                 console.error(err, 'failed to load user upload from server');
+            } finally {
+                // refresh function
+                const refreshFileRecordTimeoutId = setTimeout(fetchFileRecords, 2000);
+                fetchFileRecordTimeouts.current.push(refreshFileRecordTimeoutId);
             }
-            //refresh function
-            setTimeout(async () => {
-                refreshFileRecords.current.inProgress = true;
-                await fetchFileRecords();
-                refreshFileRecords.current.inProgress = false;
-            }, 2000);
+
         }
     }
 
-    useMemo(() => {
-        isMounted.current = true;
-        fetchFileRecords();
-    }, []);
+    // useMemo(() => {
+
+    //     fetchFileRecords();
+    // }, []);
 
     useEffect(() => {
-        isMounted.current = false;
+        isMounted.current = true;
+        fetchFileRecords();
+        return () => {
+            isMounted.current = false;
+        }
     }, []);
 
     const getFileViews = () => {
         if (Object.keys(filesRecords).length == 0) return (<></>);
         else {
             const fileViews = [];
-            console.log(filesRecords);
+            // console.log(Object.keys(filesRecords));
             Object.keys(filesRecords)
                 .forEach(fileRecordId => {
                     fileViews.push(
